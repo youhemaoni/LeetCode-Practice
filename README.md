280176b464aafb9858b0ac9e9312db6341b78152
wjf <wangjianfeng@sengled.com>
2022-09-02 11:20:20 +0800

    首次提交

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # LeetCode-Practice
-LeetCode-Practice
+LeetCode Practice
---
+++ leetcode-java/src/main/java/com/cici/leetcode/practice/J0001_TWO_SUM/TwoSum.java
@@ -0,0 +1,11 @@
+package com.cici.leetcode.practice.J0001_TWO_SUM;
+
+/**
+ * @Classname TwoSum
+ * @Description TODO
+ * @Date 2022/9/2 11:10
+ * @Created by wjf
+ */
+public class TwoSum {
+
+}
