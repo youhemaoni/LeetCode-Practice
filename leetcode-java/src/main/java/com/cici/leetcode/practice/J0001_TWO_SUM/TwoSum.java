51cbd21ea50de28c77a848f942aa7dee2a9ac7f5
wjf <wangjianfeng@sengled.com>
2022-09-06 10:19:11 +0800

    提交

--- leetcode-java/src/main/java/com/cici/leetcode/practice/J0001_TWO_SUM/TwoSum.java
+++ leetcode-java/src/main/java/com/cici/leetcode/practice/J0001_TWO_SUM/TwoSum.java
@@ -1,5 +1,8 @@
 package com.cici.leetcode.practice.J0001_TWO_SUM;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Classname TwoSum
  * @Description TODO
@@ -8,7 +11,24 @@ package com.cici.leetcode.practice.J0001_TWO_SUM;
  */
 public class TwoSum {
 
-  public static void main(String[] args) {
+  //哈希表
+  public int[] solution1(int[] nums,int target)
+  {
+    Map<Integer,Integer> map = new HashMap<Integer, Integer>();
+    for(int i=0;i<nums.length;i++)
+    {
+      if(map.containsKey(target-nums[i]))
+      {
+        return new int[]{map.get(target-nums[i]),i};
+      }
+      map.put(nums[i],i);
+    }
 
+    return new int[0];
   }
+
+
+
+
+
 }
