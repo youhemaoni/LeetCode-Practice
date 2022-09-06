a925755089ee6d7ee26c788928db4953006abcc1
wjf <wangjianfeng@sengled.com>
2022-09-06 10:56:16 +0800

    提交

---
+++ leetcode-java/src/main/java/com/cici/leetcode/practice/J0002_ADD_TOW_NUMBERS/AddTowNumbers.java
@@ -0,0 +1,38 @@
+package com.cici.leetcode.practice.J0002_ADD_TOW_NUMBERS;
+
+/**
+ * @Classname AddTowNumbers
+ * @Description TODO
+ * @Date 2022/9/6 8:32
+ * @Created by Administrator
+ */
+public class AddTowNumbers {
+
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    return null;
+  }
+
+
+}
+
+
+
+
+ class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
